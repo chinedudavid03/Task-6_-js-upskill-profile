0c1a67550b7741ba864983c93bd6d4a030baab5d
David Chinedu <chinedudavid20063@gmail.com>
2025-07-21 00:01:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//Upskill profile Object
+
+const upskillprofile = {
+    name: 'David Chinedu',
+    age: 19,
+    hobbies: ['Eating', 'Watching Movies', 'Coding' ],
+
+   Introduction() {
+        console.log(`Hi, my name is ${this.name}, and my three hobbies are ${this.hobbies.join(', ')}
+    `);
+    }   
+};
+
+upskillprofile.Introduction(); 
+//Second hobby
+console.log('My second best hobby is', upskillprofile.hobbies[1]);
+
+function greetUser (callback) {
+    console.log('Hi you are specially welcome, am Bruce and i will be your program Coordinator for the upskill program.')
+    callback();
+}
+
+function Booster() {
+    console.log('So are you ready to embark on this journey, if yes give me a JIGGY!!!!!!');
+}
+
+greetUser(Booster);
